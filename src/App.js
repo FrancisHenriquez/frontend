02d3c047d87c6e76e09dedb5195e3cc0a34c0bd4
Francis Henriquez Matos <francishenriquez07@gmail.com>
2023-08-30 14:59:29 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
 import './App.css';
+import { Users } from './users/pages/Users';
+import { NewPlace } from './places/pages/NewPlace';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <Routes>
+      
+      <Route path='/users' element={<Users/>}/>
+      <Route path='/newplace' element={<NewPlace/>}/>
+      <Route path="/*" element={ <Users />} />
+      </Routes>
+     
+        
+      
+    </Router>
+    
   );
 }
 
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NewPlace = () => {
+  return (
+    <h1>Fran si e rapa su madre</h1>
+  )
+}
