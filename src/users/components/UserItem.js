5f8d9956424b77731828f9635cda37f45371fcf3
Francis Henriquez Matos <francishenriquez07@gmail.com>
2023-09-04 14:29:50 -0400

    card for user added

--- src/users/components/UserItem.js
+++ src/users/components/UserItem.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import { Avatar } from '../../shared/components/UIElemnts/Avatar'
 import './UserItem'
+import {Card} from '../../shared/components/UIElemnts/Card'
 
 export const UserItem = props => {
   return (
     <li className="user-item">
-    <div className="user-item__content">
+    
+      <Card className = "user-item__content">
+      <Link to= {`/${props.id}/places`}>
       <div className="user-item__image">
-        <img src={props.image} alt={props.name} />
+        <Avatar image = {props.image} alt = {props.name}/>
       </div>
       <div className="user-item__info">
         <h2>{props.name}</h2>
@@ -14,7 +19,9 @@ export const UserItem = props => {
           {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
         </h3>
       </div>
-    </div>
+      </Link>
+      </Card>
+   
   </li>
   )
 }
